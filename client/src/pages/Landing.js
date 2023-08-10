e5086902952c60d60e8118ecf7d7b9f4df707f7d
Ishant Dhamija <idhamija9@gmail.com>
2023-08-10 23:05:03 +0530

    update description text on landing page

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -22,10 +22,9 @@ const Landing = () => {
               job <span>tracking</span> app
             </h1>
             <p>
-              I'm baby wayfarers hoodie next level taiyaki brooklyn cliche blue
-              bottle single-origin coffee chia. Aesthetic post-ironic venmo,
-              quinoa lo-fi tote bag adaptogen everyday carry meggings +1 brunch
-              narwhal.
+              A portal for the candidates who are applying for jobs at various
+              organisations and want to keep track of the application process at
+              various organisations.
             </p>
 
             {/* <Link to="/register" className="btn btn-hero">
